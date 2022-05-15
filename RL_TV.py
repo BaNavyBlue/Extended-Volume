f00d2755e55491553642c93909776f870b0313f2
BaNavyBlue <abajor@ucsc.edu>
2022-05-14 22:13:49 -0700

    Cleaned up some code to avoid processing too large of data types

--- RL_TV.py
+++ RL_TV.py
@@ -32,51 +32,28 @@ def RL_TV(im_extVol, otf, inner_iter, TV_reg, Nz, Ny, Nx):
 
         for k in range(inner_iter):
             if k > 1:
-                # print('J4[0,:].size: ' + str(J4[0,:].size) + ' J4[1,:].size: ' + str(J4[1,:].size))
-                # print('type(J4): ' + str(type(J4)))
-                #with cupy.cuda.Device(0):
-                
-                numer = cupy.sum(J4[0,:].conj()*J4[1,:])
-              
-                denom = (cupy.sum(J4[1,:].conj()*J4[1,:]) + epsilon)
-
-                lamb_duh = (numer)/(denom)
-                # print(lamb_duh.dtype)
-                
-                min_lamb = cupy.array([lamb_duh.get(), 1.0],dtype=cupy.float32)
-                min_lamb = cupy.nanmin((min_lamb))
-                max_lamb = cupy.array([min_lamb.get(), 0.0], dtype=cupy.float32)
-                lamb_duh = cupy.nanmax(max_lamb) #stability enforcement
+                lamb_duh = (cupy.sum(J4[0,:].conj()*J4[1,:]))/((cupy.sum(J4[1,:].conj()*J4[1,:]) + epsilon))
+                lamb_duh = max(min(lamb_duh, 1),0)
                 
             # print('lamb_duh:' + str(lamb_duh))
 
             Y = cupy.maximum(J2 + lamb_duh*(J2 - J3), 0) # plus positivity constraint
-            # print('Y.size: ' + str(Y.size) + ' Y.shape: ' + str(Y.shape))
-            # print('J2.shape: ' +  str(J2.shape))
 
             # 3.b Make core for the LR estimation
             #with cupy.cuda.Device(0):
             ReBlurred = cupy.real(fft.ifftn(otf*fft.fftn(Y))).astype(cupy.float32)
-            #print(ReBlurred)
-            #print(epsilon)
+
             ReBlurred = cupy.maximum(ReBlurred, epsilon)
-            #print('ReBlurred.size: ' + str(ReBlurred.size))
-            # print(otf.dtype)
-            # print(otf.shape)
-            # print(otf[19 ,47:79, 47:79])
-            # plt.figure(k)
-            # plt.imshow(ReBlurred[19,:,:].get())
 
             ImRatio = wI/ReBlurred + epsilon
             #print('ImRatio.size: ' + str(ImRatio.size))
 
-            Ratio = cupy.real(fft.ifftn(cupy.conj(otf)*fft.fftn(ImRatio)))
+            Ratio = cupy.real(fft.ifftn(cupy.conj(otf)*fft.fftn(ImRatio))).astype(cupy.float32)
 
             if TV_reg != 0: # total variation regularization
                 TV_term = computeTV(J2, TV_reg, Nz, Ny, Nx)
                 Ratio = Ratio/TV_term
-            # plt.figure(k)
-            # plt.imshow(Ratio[19,:,:].get())
+
             del ImRatio
             del ReBlurred
 
@@ -84,56 +61,54 @@ def RL_TV(im_extVol, otf, inner_iter, TV_reg, Nz, Ny, Nx):
 
             J3 = cupy.copy(J2)
             J2 = cupy.maximum(Y*Ratio, 0)
-            #Jtemp = J2 - Y
-            #print('J4[:,0].shape: ' + str(J4[:,0].shape) )
-            #print('J4[0].shape: ' + str(J4[0].shape) )
+
             J4[1,:] = cupy.copy(J4[0,:])
-            # J4[:,0] = cupy.copy(J2 - Y).get()
+
             J4[0,:] = cupy.copy(cupy.reshape((J2 - Y),(J4[0].size,), order='C'))
 
-        return J2.astype(cupy.float64)
-
+        return J2
 
 def computeTV(Image, TV_reg, Nz, Ny, Nx):
     epsilon = cupy.finfo(cupy.float32).eps
-    gx = cupy.diff(Image, 1, 2)
-    Oxp = cupy.pad(gx,((0,0),(0,0),(0,1)), mode='constant', constant_values = 0 )
-    Oxn = cupy.pad(gx, ((0,0),(0,0),(1,0)), mode='constant', constant_values = 0)
-    mx = (cupy.sign(Oxp) + cupy.sign(Oxn))/2*cupy.minimum(Oxp, Oxn)
-    mx = cupy.maximum(mx, epsilon)
-    Dx = Oxp/cupy.sqrt(Oxp**2 + mx**2)
-    DDx = cupy.diff(Dx, 1 ,2)
-    DDx = cupy.pad(DDx, ((0,0),(0,0),(1,0)), mode='constant', constant_values = 0)
+    with cupy.cuda.Device(0):
+        gx = cupy.diff(Image, 1, 2)
+        Oxp = cupy.pad(gx,((0,0),(0,0),(0,1)), mode='constant', constant_values = 0 )
+        Oxn = cupy.pad(gx, ((0,0),(0,0),(1,0)), mode='constant', constant_values = 0)
+        mx = (cupy.sign(Oxp) + cupy.sign(Oxn))/2*cupy.minimum(Oxp, Oxn)
+        mx = cupy.maximum(mx, epsilon)
+        Dx = Oxp/cupy.sqrt(Oxp**2 + mx**2)
+        DDx = cupy.diff(Dx, 1 ,2)
+        DDx = cupy.pad(DDx, ((0,0),(0,0),(1,0)), mode='constant', constant_values = 0)
 
-    del gx
-    del Oxp
-    del Oxn
-    del mx
-    del Dx
-    
-    gy = cupy.diff(Image, 1, 1)
-    Oyp = cupy.pad(gy, ((0,0),(0,1),(0,0)), mode='constant', constant_values = 0)
-    Oyn = cupy.pad(gy, ((0,0),(1,0),(0,0)), mode='constant', constant_values = 0)
-    my = (cupy.sign(Oyp) + cupy.sign(Oyn))/2*cupy.minimum(Oyp, Oyn)
-    my = cupy.maximum(my, epsilon)
-    Dy = Oyp/cupy.sqrt(Oyp**2 + my**2)
-    DDy = cupy.diff(Dy, 1, 1)
-    DDy = cupy.pad(DDy, ((0,0),(1,0), (0,0)), mode='constant', constant_values = 0)
+        del gx
+        del Oxp
+        del Oxn
+        del mx
+        del Dx
+        
+        gy = cupy.diff(Image, 1, 1)
+        Oyp = cupy.pad(gy, ((0,0),(0,1),(0,0)), mode='constant', constant_values = 0)
+        Oyn = cupy.pad(gy, ((0,0),(1,0),(0,0)), mode='constant', constant_values = 0)
+        my = (cupy.sign(Oyp) + cupy.sign(Oyn))/2*cupy.minimum(Oyp, Oyn)
+        my = cupy.maximum(my, epsilon)
+        Dy = Oyp/cupy.sqrt(Oyp**2 + my**2)
+        DDy = cupy.diff(Dy, 1, 1)
+        DDy = cupy.pad(DDy, ((0,0),(1,0), (0,0)), mode='constant', constant_values = 0)
 
-    del gy
-    del Oyp
-    del Oyn
-    del my
-    del Dy
+        del gy
+        del Oyp
+        del Oyn
+        del my
+        del Dy
 
-    TV_term = 1 - (DDx + DDy)*TV_reg
+        TV_term = 1 - (DDx + DDy)*TV_reg
 
 
-    del DDx
-    del DDy
+        del DDx
+        del DDy
 
-    TV_term = cupy.maximum(TV_term, epsilon)
-    #print('Size TV_term: ' + str(TV_term.size) + ' Shape TV_term: ' + str(TV_term.shape))
+        TV_term = cupy.maximum(TV_term, epsilon)
+    #print('Size TV_term: ' + str(TV_term.size) + ' Shape TV_term: ' + str(TV_term.shape) + ' type: ' + str(TV_term.dtype))
     #plt.figure(random.randint(0,100))
     #plt.imshow(TV_term[19,:,:].get())
     #print(TV_term[19,0:31,0:31])
